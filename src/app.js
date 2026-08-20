1b9ae022a388bffee07e62b01a13f5e82e3fd088
sundramsoni8899 <sundramsoni5002soni@gmail.com>
2026-08-21 00:38:31 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -20,4 +20,13 @@ app.use(express.static('public'))
 
 app.use(cookieParser())
 
+//routes
+
+import userRouter from './routes/user.routes.js';
+
+
+// routes decalaration
+
+app.use("/api/v1/users", userRouter)
+
 export { app }  
