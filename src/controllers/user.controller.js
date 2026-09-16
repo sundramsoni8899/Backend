0b616ad931031395f7669363c968ed2324fcb609
sundramsoni8899 <sundramsoni5002soni@gmail.com>
2026-09-16 22:46:56 +0530

    fix: update logoutUser to unset refreshToken instead of setting it to undefined

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,8 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 
+import mongoose from "mongoose";
+
 const generateAccessAndRefereshTokens = async(userId)=>{
     try{
         const user = await User.findById(userId)
@@ -162,8 +164,8 @@ const logoutUser = asyncHandler(async(req, res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
